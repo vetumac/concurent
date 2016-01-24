840a64ca3b6ec1fbac9899782feda7c098e73ed7
Alexey Suslov <alexei.suslov@gmail.com>
2016-01-24 13:09:06 +0300

    CyclicBarier

--- src/by/vetumac/learn/concurent/Operations.java
+++ src/by/vetumac/learn/concurent/Operations.java
@@ -1,23 +1,38 @@
 package by.vetumac.learn.concurent;
 
+import javafx.util.Duration;
+
+import java.time.Instant;
 import java.util.Random;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class Operations {
 
     public static void main(String[] args) throws InterruptedException {
+        final int N = 10;
         final Account a = new Account(1000);
         final Account b = new Account(2000);
         final Random random = new Random();
         final ExecutorService executorService = Executors.newFixedThreadPool(3);
         final ScheduledExecutorService schedulerService = Executors.newScheduledThreadPool(1);
+        final CountDownLatch countDownLatchAB = new CountDownLatch(0);
+        final CyclicBarrier cyclicBarrierAB = new CyclicBarrier(1);
+        final Instant before;
+        final Instant[] after = new Instant[1];
+        final CyclicBarrier cyclicBarrierBA = new CyclicBarrier(N, () -> after[0] = Instant.now());
 
-        for (int i = 0; i < 10; i++) {
-            executorService.submit(new Transfer(a, b, random.nextInt(400)));
+        for (int i = 0; i < N; i++) {
+            executorService.submit(new Transfer(a, b, random.nextInt(400), countDownLatchAB, cyclicBarrierAB));
         }
+
+        final CountDownLatch countDownLatchBA = new CountDownLatch(1);
+
+        for (int i = 0; i < N; i++) {
+            executorService.submit(new Transfer(b, a, random.nextInt(400), countDownLatchBA, cyclicBarrierBA));
+        }
+
+        before = Instant.now();
+        countDownLatchBA.countDown();
 
         schedulerService.scheduleAtFixedRate(() -> {
             System.out.println("A - " + a.getFailCounter());
@@ -25,8 +40,9 @@ public class Operations {
         }, 1, 1, TimeUnit.SECONDS);
 
         executorService.shutdown();
-        executorService.awaitTermination(11, TimeUnit.SECONDS);
+        executorService.awaitTermination(1100, TimeUnit.SECONDS);
 
+        System.out.println(new Duration(after[0].toEpochMilli() - before.toEpochMilli()));
     }
 }
 
--- src/by/vetumac/learn/concurent/Transfer.java
+++ src/by/vetumac/learn/concurent/Transfer.java
@@ -2,6 +2,8 @@ package by.vetumac.learn.concurent;
 
 import java.util.Random;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -14,11 +16,15 @@ public class Transfer implements Callable<Boolean> {
     private final Account accTo;
     private final int amount;
     private final Random random;
+    private final CountDownLatch countDownLatch;
+    private final CyclicBarrier cyclicBarrier;
 
-    public Transfer(Account accFrom, Account accTo, int amount) {
+    public Transfer(Account accFrom, Account accTo, int amount, CountDownLatch countDownLatch, CyclicBarrier cyclicBarrier) {
         this.accFrom = accFrom;
         this.accTo = accTo;
         this.amount = amount;
+        this.cyclicBarrier = cyclicBarrier;
+        this.countDownLatch = countDownLatch;
         this.random = new Random();
     }
 
@@ -26,40 +32,45 @@ public class Transfer implements Callable<Boolean> {
     public Boolean call() throws Exception {
         final int WAIT_SEC = 10;
 
+        countDownLatch.await();
+
         System.out.println("Transfer start");
+        Boolean needRepeat = true;
 
         try {
-            if (accFrom.getLock().tryLock(WAIT_SEC, TimeUnit.SECONDS)) {
-                System.out.println("Synchronized From");
-                Thread.sleep(random.nextInt(5000));
-                if (accFrom.getBalance() < amount) throw new InsufficiepnFoundException();
-                try {
-                    if (accTo.getLock().tryLock(WAIT_SEC, TimeUnit.SECONDS)) {
-                        try {
-                            System.out.println("Synchronized To");
-                            Thread.sleep(random.nextInt(10000));
-                            accFrom.withdraw(amount);
-                            accTo.deposit(amount);
-                        } finally {
-                            accTo.getLock().unlock();
+            while (needRepeat) {
+                if (accFrom.getLock().tryLock(WAIT_SEC, TimeUnit.SECONDS)) {
+                    System.out.println("Synchronized From");
+                    Thread.sleep(random.nextInt(5000));
+                    if (accFrom.getBalance() < amount) throw new InsufficiepnFoundException();
+                    try {
+                        if (accTo.getLock().tryLock(WAIT_SEC, TimeUnit.SECONDS)) {
+                            try {
+                                System.out.println("Synchronized To");
+                                Thread.sleep(random.nextInt(10000));
+                                accFrom.withdraw(amount);
+                                accTo.deposit(amount);
+                                needRepeat = false;
+                            } finally {
+                                accTo.getLock().unlock();
+                            }
+                        } else {
+                            accFrom.incFailedTransferCount();
+                            accTo.incFailedTransferCount();
                         }
-                    } else {
-                        accFrom.incFailedTransferCount();
-                        accTo.incFailedTransferCount();
-                        return false;
+                    } finally {
+                        accFrom.getLock().unlock();
                     }
-                } finally {
-                    accFrom.getLock().unlock();
+                } else {
+                    accFrom.incFailedTransferCount();
+                    accTo.incFailedTransferCount();
                 }
-            } else {
-                accFrom.incFailedTransferCount();
-                accTo.incFailedTransferCount();
-                return false;
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
+        cyclicBarrier.await();
 
         System.out.println("Transfer complete");
         return true;
